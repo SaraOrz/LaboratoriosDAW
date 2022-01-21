7f20f0f1b1348a34f2f2d6d31e0986ad427d7796
SaraOrz <a01704432@tec.mx>
2022-01-21 17:17:18 -0600

    CopyL4L5

--- Lab4/js/script.js
+++ Lab4/js/script.js
@@ -1,67 +1,47 @@
-//1 Obtener las referencias de los elementos a interactuar
- 
-let post = document.getElementById("ButtonPost")
-let clear = document.getElementById("ButtonClear")
-let mark = document.getElementById("ButtonMark")
-let del = document.getElementById("ButtonDelete")
- 
-// Registrar el Evento
- 
-post.addEventListener("click",TodoPost)
-clear.addEventListener("click",TodoClear)
-mark.addEventListener("click",TodoMark)
-del.addEventListener("click",TodoDel)
- 
-function TodoPost(e) {
-e.preventDefault()
- 
-let todo = document.getElementById("todoText").value
-let list = document.getElementById("todoList")
- 
-let div = document.createElement("div")
-let input = document.createElement("input")
-let label = document.createElement("label")
- 
-input.type = "checkbox"
-input.name = "todo"
- 
-label.textContent = todo
- 
-div.appendChild(input)
-div.appendChild(label)
- 
-list.append(div)
- 
+let post  = document.getElementById("ButtonPost");
+let clear  = document.getElementById("ButtonClear");
+let mark  = document.getElementById("ButtonMark");
+let del  = document.getElementById("ButtonDelete");
+
+post.addEventListener("click",TodoPost);
+clear.addEventListener("click",TodoClear);
+mark.addEventListener("click",TodoMark);
+del.addEventListener("click",TodoDel);
+
+
+function TodoPost(e){
+e.preventDefault();
+// Necesito obtener el valor de la caja
+var todo = document.getElementById("todoText").value;
+var list = document.getElementById("todoList");
+
+let currentListHTML = list.innerHTML;
+list.innerHTML = currentListHTML + '<input type = "checkbox" name="todo" /> ' + todo + '<br>';
+
+// Clear To Do input 
+document.getElementById("todoText").value = ''
+
 }
- 
-function TodoClear(){
- 
-    let todos = document.getElementsByName("todo")
-    for(let i = 0; i< todos.length; i++){
-        todos[i].checked = false
+
+
+function TodoClear(e){
+    e.preventDefault();
+    var todos = document.getElementsByName("todo");
+    for(var i = 0; i < todos.length; i++){
+        todos[i].checked = false;
     }
- 
 }
- 
-function TodoMark() {
- 
-    let todos = document.getElementsByName("todo")
-    for(let i = 0; i< todos.length; i++){
-        todos[i].checked = true
+
+function TodoMark(e){
+    e.preventDefault();
+    var todos = document.getElementsByName("todo");
+    for(var i = 0; i < todos.length; i++){
+        todos[i].checked = true;
     }
- 
 }
- 
-function TodoDel() {
- 
-    let todos = document.getElementsByName("todo")
- 
-    for(let i = 0; i< todos.length; i++){
-  
-    if (todos[i].checked){
-        todos[i].parentElement.remove()
-    }
- 
-    }
- 
+
+function TodoDel(e){
+    e.preventDefault();
+    var list = document.getElementById("todoList");
+    list.innerHTML = "";
 }

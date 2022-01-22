ed82d56599bdb021a0fad3ebcfc6ca4b40db40ec
SaraOrz <a01704432@tec.mx>
2022-01-21 20:45:19 -0600

    Lab5

--- Lab5/js/script.js
+++ Lab5/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let post  = document.getElementById("ButtonPost");
 let clear  = document.getElementById("ButtonClear");
 let mark  = document.getElementById("ButtonMark");
@@ -45,52 +44,4 @@ function TodoDel(e){
     e.preventDefault();
     var list = document.getElementById("todoList");
     list.innerHTML = "";
-=======
-let post  = document.getElementById("ButtonPost");
-let clear  = document.getElementById("ButtonClear");
-let mark  = document.getElementById("ButtonMark");
-let del  = document.getElementById("ButtonDelete");
-
-post.addEventListener("click",TodoPost);
-clear.addEventListener("click",TodoClear);
-mark.addEventListener("click",TodoMark);
-del.addEventListener("click",TodoDel);
-
-
-function TodoPost(e){
-e.preventDefault();
-// Necesito obtener el valor de la caja
-var todo = document.getElementById("todoText").value;
-var list = document.getElementById("todoList");
-
-let currentListHTML = list.innerHTML;
-list.innerHTML = currentListHTML + '<input type = "checkbox" name="todo" /> ' + todo + '<br>';
-
-// Clear To Do input 
-document.getElementById("todoText").value = ''
-
-}
-
-
-function TodoClear(e){
-    e.preventDefault();
-    var todos = document.getElementsByName("todo");
-    for(var i = 0; i < todos.length; i++){
-        todos[i].checked = false;
-    }
-}
-
-function TodoMark(e){
-    e.preventDefault();
-    var todos = document.getElementsByName("todo");
-    for(var i = 0; i < todos.length; i++){
-        todos[i].checked = true;
-    }
-}
-
-function TodoDel(e){
-    e.preventDefault();
-    var list = document.getElementById("todoList");
-    list.innerHTML = "";
->>>>>>> b4830fead0ecbb2782e4a852f3ba83b9060d8723
 }

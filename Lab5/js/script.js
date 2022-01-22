11506457efb68760cf6149513c5c0a17441684bf
SaraOrz <a01704432@tec.mx>
2022-01-21 23:40:48 -0600

    Lab5

--- Lab5/js/script.js
+++ Lab5/js/script.js
@@ -1,42 +1,40 @@
-var qtodo = 0
-
 // Post
-$("#ButtonPost").on("click", function(event) {
-    event.preventDefault()
-    let todo = $("#todoText").val()
+$("#ButtonPost").on("click", (event) => {
+        event.preventDefault()
+        let todo = $("#todoText").val()
 
-    if (todo != ""){
-        
-        $("#todoList").prepend('<input type="checkbox" /> ' + todo + '<br />')
-        
-        qtodo++
-        $("#todoText").val("")
-    }
-})
+        if (todo != "") {
+
+            $("#todoList").prepend('<input type="checkbox" /> ' + todo + '<br />')
+
+
+            $("#todoText").val("")
+        }
+    })
 
 // Clear All
-$("#ButtonClear").on("click", function(event) {
-    event.preventDefault()
-    let list = $("#todoList")
+$("#ButtonClear").on("click", (event) => {
+        event.preventDefault()
+        let list = $("#todoList")
 
-    $(":checkbox").prop('checked', false);
-})
+        $(":checkbox").prop('checked', false)
+    })
 
 // Mark All
-$("#ButtonMark").on("click", function(event) {
-    event.preventDefault()
-    let list = $("#todoList")
-    
-    $(":checkbox").prop('checked', true);
-    
-})
+$("#ButtonMark").on("click", (event) => {
+        event.preventDefault()
+        let list = $("#todoList")
+
+        $(":checkbox").prop('checked', true)
+
+    })
 
 // Delete All
-$("#ButtonDelete").on("click", function(event) {
-    event.preventDefault()
-    $("#todoList").html("")
-    qtodo = 0
-})
+$("#ButtonDelete").on("click", (event) => {
+        event.preventDefault()
+        $("#todoList").html("")
+
+    })
 
 //------
 
